8fe600fa50b345200bbf786d49c79e467e8218f8
Bilgekaan Yilmaz <bilgekaan.yilmaz36@gmail.com>
2022-01-17 10:08:29 +0100

    add http server via google firebase

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,24 +11,26 @@ export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
   
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Tasty Schnitzel', 
-            'A super-tasty Schnitzel - just awesome', 
-            'https://c.pxhere.com/images/22/4a/6321d069b3386c120e0c860dc56a-1620832.jpg!d',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]),
-        new Recipe(
-            'Big Fat Burger', 
-            'What else you need to say?', 
-            'https://c.pxhere.com/images/22/4a/6321d069b3386c120e0c860dc56a-1620832.jpg!d',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1)
-            ])
-      ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Tasty Schnitzel', 
+    //         'A super-tasty Schnitzel - just awesome', 
+    //         'https://c.pxhere.com/images/22/4a/6321d069b3386c120e0c860dc56a-1620832.jpg!d',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French Fries', 20)
+    //         ]),
+    //     new Recipe(
+    //         'Big Fat Burger', 
+    //         'What else you need to say?', 
+    //         'https://c.pxhere.com/images/22/4a/6321d069b3386c120e0c860dc56a-1620832.jpg!d',
+    //         [
+    //             new Ingredient('Buns', 2),
+    //             new Ingredient('Meat', 1)
+    //         ])
+    //   ];
+
+    private recipes: Recipe[] = [];
 
       constructor(private slService: ShoppingListService) {}
 
@@ -65,6 +67,9 @@ export class RecipeService {
           this.recipesChanged.next(this.recipes.slice());
       }
 
-
+      setRecipes(recipes: Recipe[]){
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+      }
 
 }
